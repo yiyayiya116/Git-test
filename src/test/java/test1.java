31b47f908a0dcd53028ca74744cc0bba17a4af46
yiyayiya116 <87699106+yiyayiya116@users.noreply.github.com>
2021-07-22 09:25:50 +0800

    pull master

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -7,5 +7,6 @@ public class test1 {
         System.out.println("master-merge");
         System.out.println("hot-fix-merge");
         System.out.println("push master");
+        System.out.println("pull master");
     }
 }
